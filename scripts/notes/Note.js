7ede56f5d0f15ccb3fe5c6fdcef7386c2221186c
Martins Lazdans <marrtins@dqdp.net>
2020-11-26 08:20:35 +0200

    Cleanup

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -56,15 +56,9 @@ class Note {
 		let fName = `${this.constructor.name}.save()`;
 		QDEB&&console.debug(`${fName} - saving...`);
 
-		// Prepare data to save. We do not want all note properties saved
-		let data = this.get();
-
-		return saver(data).then(() => {
-			this.exists = true;
+		return saver(this.get()).then(() => {
 			QDEB&&console.debug(`${fName} - saved!`);
-		}).catch(e => {
-			QDEB&&console.debug(`${fName} - failure: ${e.message}`);
-			return Promise.reject(e);
+			this.exists = true;
 		});
 	}
 
@@ -73,11 +67,8 @@ class Note {
 		QDEB&&console.debug(`${fName} - deleting...`);
 
 		return deleter().then(() => {
-			this.exists = false;
 			QDEB&&console.debug(`${fName} - deleted!`);
-		}).catch(e => {
-			QDEB&&console.debug(`${fName} - failure: ${e.message}`);
-			return Promise.reject(e);
+			this.exists = false;
 		});
 	}
 }
