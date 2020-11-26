6225300f5c71fca00e81a06376942eb1c8834b4f
Martins Lazdans <marrtins@dqdp.net>
2020-11-26 06:21:38 +0200

    Wrap exceptions inside ExtensionError

--- api/qnote.js
+++ api/qnote.js
@@ -54,82 +54,93 @@ var qnote = class extends ExtensionCommon.ExtensionAPI {
 		return {
 			qnote: {
 				async saveNote(root, keyId, note){
-					var file = noteFile(root, keyId);
-					let data = JSON.stringify(note);
+					try {
+						var file = noteFile(root, keyId);
+						let data = JSON.stringify(note);
 
-					let tempFile = file.parent.clone();
-					tempFile.append("~" + file.leafName + ".tmp");
-					tempFile.createUnique(tempFile.NORMAL_FILE_TYPE, parseInt("0660",8));
+						let tempFile = file.parent.clone();
+						tempFile.append("~" + file.leafName + ".tmp");
+						tempFile.createUnique(tempFile.NORMAL_FILE_TYPE, parseInt("0660",8));
 
-					let fileOutStream = Cc['@mozilla.org/network/file-output-stream;1'].createInstance(Ci.nsIFileOutputStream);
-					fileOutStream.init(tempFile, 2, 0x200, false); // Opens for writing only
+						let fileOutStream = Cc['@mozilla.org/network/file-output-stream;1'].createInstance(Ci.nsIFileOutputStream);
+						fileOutStream.init(tempFile, 2, 0x200, false); // Opens for writing only
 
-					var con = Cc["@mozilla.org/intl/converter-output-stream;1"].createInstance(Ci.nsIConverterOutputStream);
-					con.init(fileOutStream, "utf-8", 0, 0xFFFD); // U+FFFD = replacement character
-					con.writeString(data);
-					con.close();
+						var con = Cc["@mozilla.org/intl/converter-output-stream;1"].createInstance(Ci.nsIConverterOutputStream);
+						con.init(fileOutStream, "utf-8", 0, 0xFFFD); // U+FFFD = replacement character
+						con.writeString(data);
+						con.close();
 
-					// fileOutStream.init(tempFile, 2, 0x200, false); // Opens for writing only
-					// fileOutStream.write(data, data.length);
-					fileOutStream.close();
+						// fileOutStream.init(tempFile, 2, 0x200, false); // Opens for writing only
+						// fileOutStream.write(data, data.length);
+						fileOutStream.close();
 
-					tempFile.moveTo(null, file.leafName);
-
-					return true;
+						tempFile.moveTo(null, file.leafName);
+					} catch(e) {
+						throw new ExtensionError(e.message);
+					}
 				},
 				async deleteNote(root, keyId){
-					var file = getExistingNoteFile(root, keyId);
-
-					file.remove(false);
-
-					return true;
+					try {
+						var file = getExistingNoteFile(root, keyId);
+						file.remove(false);
+					} catch(e) {
+						throw new ExtensionError(e.message);
+					}
 				},
 				async loadNote(root, keyId){
-					var file = getExistingNoteFile(root, keyId);
-
-					if(!file){
-						return false;
-					}
-
-					var fileInStream = Cc['@mozilla.org/network/file-input-stream;1'].createInstance(Ci.nsIFileInputStream);
-					//var fileScriptableIO = Cc['@mozilla.org/scriptableinputstream;1'].createInstance(Ci.nsIScriptableInputStream);
-					fileInStream.init(file, 0x01, parseInt("0444", 8), null);
-
-					var con = Cc["@mozilla.org/intl/converter-input-stream;1"].createInstance(Ci.nsIConverterInputStream);
-					con.init(fileInStream, "utf-8", 0, 0xFFFD); // U+FFFD = replacement character
-
-					//fileScriptableIO.init(fileInStream);
-					var data = '';
-					var str = {};
-					while (con.readString(4096, str) != 0) {
-						data += str.value;
-					}
-
-					//fileScriptableIO.close();
-					con.close();
-					fileInStream.close();
-
 					try {
-						return JSON.parse(data);
-					} catch {
-						return null;
+						var file = getExistingNoteFile(root, keyId);
+
+						if(!file){
+							return false;
+						}
+
+						var fileInStream = Cc['@mozilla.org/network/file-input-stream;1'].createInstance(Ci.nsIFileInputStream);
+						//var fileScriptableIO = Cc['@mozilla.org/scriptableinputstream;1'].createInstance(Ci.nsIScriptableInputStream);
+						fileInStream.init(file, 0x01, parseInt("0444", 8), null);
+
+						var con = Cc["@mozilla.org/intl/converter-input-stream;1"].createInstance(Ci.nsIConverterInputStream);
+						con.init(fileInStream, "utf-8", 0, 0xFFFD); // U+FFFD = replacement character
+
+						//fileScriptableIO.init(fileInStream);
+						var data = '';
+						var str = {};
+						while (con.readString(4096, str) != 0) {
+							data += str.value;
+						}
+
+						//fileScriptableIO.close();
+						con.close();
+						fileInStream.close();
+
+						try {
+							return JSON.parse(data);
+						} catch {
+							return null;
+						}
+					} catch(e) {
+						throw new ExtensionError(e.message);
 					}
 				},
 				async getAllKeys(root) {
-					var file = new FileUtils.File(root);
-					var eFiles = file.directoryEntries;
-					var notes = [];
+					try {
+						var file = new FileUtils.File(root);
+						var eFiles = file.directoryEntries;
+						var notes = [];
 
-					while (eFiles.hasMoreElements()) {
-						var o = eFiles.getNext().QueryInterface(Components.interfaces.nsIFile);
+						while (eFiles.hasMoreElements()) {
+							var o = eFiles.getNext().QueryInterface(Components.interfaces.nsIFile);
 
-						var fileName = decodeFileName(o.leafName);
-						if(fileName.substring(fileName.length - 6) === '.qnote'){
-							notes.push(fileName.substring(0, fileName.length - 6));
+							var fileName = decodeFileName(o.leafName);
+							if(fileName.substring(fileName.length - 6) === '.qnote'){
+								notes.push(fileName.substring(0, fileName.length - 6));
+							}
 						}
-					}
 
-					return notes;
+						return notes;
+					} catch(e) {
+						throw new ExtensionError(e.message);
+					}
 				}
 			}
 		}
