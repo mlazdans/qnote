ecc88ba713346b9d0ba36efaec7bd1ad8952ccc7
Martins Lazdans <marrtins@dqdp.net>
2024-08-29 19:04:09 +0300

    Cleanup

--- src/api/xnote.ts
+++ src/api/xnote.ts
@@ -1,7 +1,3 @@
-var ExtensionUtils = ChromeUtils.import("resource://gre/modules/ExtensionUtils.jsm").ExtensionUtils;
-var { ExtensionError } = ExtensionUtils;
-var { ExtensionParent } = ChromeUtils.import("resource://gre/modules/ExtensionParent.jsm");
-
 var api = ChromeUtils.importESModule("resource://qnote/modules-exp/api.mjs");
 
 var xnote = class extends ExtensionCommon.ExtensionAPI {
