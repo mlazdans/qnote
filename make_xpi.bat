a668fe7b3c64f899aa074a1e969ae751440117f9
Martins Lazdans <marrtins@dqdp.net>
2024-09-19 18:11:25 +0300

    Adjust paths

--- make_xpi.bat
+++ make_xpi.bat
@@ -1,7 +1,7 @@
 @echo off
 
-for /f %%i in ('jq -r .version qnote/dist/release/manifest.json') do set VERS=%%i
-set E=qnote-%VERS%.xpi
+for /f %%i in ('jq -r .version dist/release/manifest.json') do set VERS=%%i
+set E=qnote-%VERS%.xpi.zip
 
 if exist %E% (
 	echo %E% exists, removing
@@ -9,7 +9,7 @@ if exist %E% (
 	rem file exists
 )
 
-"C:\Program Files\7-Zip\7z.exe" a %E% .\qnote\dist\release\* > nul
+"C:\Program Files\7-Zip\7z.exe" a %E% .\dist\release\* > nul
 
 if errorlevel 1 (
 	echo 7z failure %errorlevel%
